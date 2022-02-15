83a0122ee4e8d8a42fae6e0a2b085332acd901b6
Stamka <83986532+Stamka@users.noreply.github.com>
2022-02-15 09:54:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function run(){
                 domain: 'jumoreski',
                 offset: 100*i,
                 count: process.argv[2],
-                access_token: 'c4b2c5373bcde61383fc6b8b46868d12c6d9a9741c0107b450e4691faa9a82c3f22d3b50e70210ac616ef',
+                access_token: '', //insert your own go api
                 v: '5.131',
                 extended: false
             },
